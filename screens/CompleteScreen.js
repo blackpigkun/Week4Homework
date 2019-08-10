05539504663e7eb15aebd4320449a56917b56ed4
blackpigkun <47916538+blackpigkun@users.noreply.github.com>
2019-08-10 15:41:01 +0700

    Add files via upload

---
+++ screens/CompleteScreen.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput, Button,
+} from 'react-native';
+import { TODOS } from '../utils/data';
+import TodoItem from '../components/TodoItem';
+export default class CompleteScreen extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+  static navigationOptions = {
+    title: 'Your Complete Todo List',
+    headerStyle: {
+      backgroundColor: '#1a1a1a',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  };
+  render() {
+    const todoList  = this.props.navigation.getParam('todoList');
+    return (<ScrollView contentContainerStyle={styles.container}>
+      {
+        todoList.map(item => {
+          if(item.status === 'Done')
+          return (
+            <TodoItem key={item.id} data={item}  />
+          );
+        })
+      }
+
+    </ScrollView>
+    )
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#0d0d0d',
+    marginTop: 15,
+    paddingVertical: 10,
+    justifyContent: 'space-around'
+  },
+  
+});
